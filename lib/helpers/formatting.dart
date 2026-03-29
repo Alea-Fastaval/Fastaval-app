c51e31054ae09d5555ee36c1339e5448fca18686
Mads Heimdal Thy <mads@heimdal-thy.dk>
2026-03-29 13:30:22 +0200

    chore: Remove UTC flag from DateTime conversion in formatting helper

--- lib/helpers/formatting.dart
+++ lib/helpers/formatting.dart
@@ -9,7 +9,7 @@ String formatTime(int? time) =>
     DateFormat('HH:mm').format(formatTimestampToDateTime(time!));
 
 DateTime formatTimestampToDateTime(int timeInUnixTime) =>
-    DateTime.fromMillisecondsSinceEpoch(timeInUnixTime * 1000, isUtc: true);
+    DateTime.fromMillisecondsSinceEpoch(timeInUnixTime * 1000);
 
 String getLanguage(String language) {
   switch (language) {
